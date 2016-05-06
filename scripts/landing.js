b90c7bc6aa643fb7737a2918a5bf3c28ea2a5a27
Erik Lopez <eriklopez0712@gmail.com>
2016-05-05 22:11:13 -0400

    Assignment 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,22 +1,23 @@
- var pointsArray = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
- 
-     var revealPoint =function(index){ 
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-     for (var i = 0; i < points.length; i++){
-         revealPoint(i);
-     }
+var revealPoint =function(point){ 
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
+     }; 
+
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
  };
 
 window.onload = function() {
+    //This trigers the animation if the screen is big enough for the user not to scroll.
     if (window.innerHeight > 950){
         animatePoints(pointsArray);
     }
+    //Calculates the distance from the top of the document to where the objects to be animated are located
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
      window.addEventListener('scroll', function(event) {
