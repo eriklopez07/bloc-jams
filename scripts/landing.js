13705dd5b9b791ac47f63e8f568115f450c05cde
Erik Lopez <eriklopez0712@gmail.com>
2016-06-28 22:19:50 -0500

    checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,4 @@
-var pointsArray = document.getElementsByClassName('point');
+/*var pointsArray = document.getElementsByClassName('point');
  
 var revealPoint =function(point){ 
         point.style.opacity = 1;
@@ -7,23 +7,55 @@ var revealPoint =function(point){
         point.style.WebkitTransform = "scaleX(1) translateY(0)";
      }; 
 
+*/
+var animatePoints = function() {
+    var revealPoint = function(){
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+    });
+};
+    $.each($('.point'), revealPoint);
 
-var animatePoints = function(points) {
+/*var animatePoints = function(points) {
     forEach(points, revealPoint);
  };
+*/
 
-window.onload = function() {
+$(window).load(function() {
     //This trigers the animation if the screen is big enough for the user not to scroll.
-    if (window.innerHeight > 950){
+    //  Vanilla JS Version
+    /*if (window.innerHeight > 950){
         animatePoints(pointsArray);
     }
+    */
+    // jQuery Version
+    if ($(window).height() > 950) {
+        animatePoints();
+    }
+    
     //Calculates the distance from the top of the document to where the objects to be animated are located
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    //  Vanilla JS Version
+    /*var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-     window.addEventListener('scroll', function(event) {
-         console.log("Current offset from the top is: " + sellingPoints.getBoundingClientRect().top + " pixels");
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-             animatePoints(pointsArray);
+    */
+    // jQuery Version
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    // Vanilla JS Version
+     /* window.addEventListener('scroll', function(event) {
+         console.log("Current offset from the top is: " + sellingPoints.getBoundingClientRect().top + " pixels"); */
+    //jQuery Version 
+    $(window).scroll(function(event) { 
+    
+    // Vanilla Version   
+    /* if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+             animatePoints(pointsArray);*/
+    //jQuery Version 
+        if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+        
+        
          }
      });
- }
+  });
